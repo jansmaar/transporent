bf02c494e611048d432e30ffe09ed79073e600f2
Nick Schreuder <nickschreudermail@gmail.com>
2016-06-29 16:18:53 +0200

    extra seeds & css changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,122 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ad.create ([{
+categorie:"Bussen",
+brand:"Mercedes",
+capacity:"600",
+license:"Rijbewijs B",
+plug:"Nvt.",
+size:"100x200x30",
+restrictions:"Niet roken & drinken",
+status:"Beschikbaar",
+title:"Mercedes Sprinter",
+description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+transmission:"Hand geschakeld",
+year:"1990",
+engine:"300pk",
+fuel:"benzine"
+  }])
+
+  Ad.create ([{
+  categorie:"Bussen",
+  brand:"Mercedes",
+  capacity:"600",
+  license:"Rijbewijs B",
+  plug:"Nvt.",
+  size:"100x200x30",
+  restrictions:"Niet roken & drinken",
+  status:"Beschikbaar",
+  title:"Mercedes Sprinter",
+  description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+  transmission:"Hand geschakeld",
+  year:"1990",
+  engine:"300pk",
+  fuel:"benzine"
+    }])
+
+Ad.create ([{
+    categorie:"Bussen",
+    brand:"Mercedes",
+    capacity:"600",
+    license:"Rijbewijs B",
+    plug:"Nvt.",
+    size:"100x200x30",
+    restrictions:"Niet roken & drinken",
+    status:"Beschikbaar",
+    title:"Mercedes Sprinter",
+    description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+    transmission:"Hand geschakeld",
+    year:"1990",
+    engine:"300pk",
+    fuel:"benzine"
+  }])
+
+  Ad.create ([{
+      categorie:"Bussen",
+      brand:"Mercedes",
+      capacity:"600",
+      license:"Rijbewijs B",
+      plug:"Nvt.",
+      size:"100x200x30",
+      restrictions:"Niet roken & drinken",
+      status:"Beschikbaar",
+      title:"Mercedes Sprinter",
+      description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+      transmission:"Hand geschakeld",
+      year:"1990",
+      engine:"300pk",
+      fuel:"benzine"
+    }])
+
+    Ad.create ([{
+        categorie:"Bussen",
+        brand:"Mercedes",
+        capacity:"600",
+        license:"Rijbewijs B",
+        plug:"Nvt.",
+        size:"100x200x30",
+        restrictions:"Niet roken & drinken",
+        status:"Beschikbaar",
+        title:"Mercedes Sprinter",
+        description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+        transmission:"Hand geschakeld",
+        year:"1990",
+        engine:"300pk",
+        fuel:"benzine"
+      }])
+
+      Ad.create ([{
+          categorie:"Bussen",
+          brand:"Mercedes",
+          capacity:"600",
+          license:"Rijbewijs B",
+          plug:"Nvt.",
+          size:"100x200x30",
+          restrictions:"Niet roken & drinken",
+          status:"Beschikbaar",
+          title:"Mercedes Sprinter",
+          description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+          transmission:"Hand geschakeld",
+          year:"1990",
+          engine:"300pk",
+          fuel:"benzine"
+        }])
+
+        Ad.create ([{
+            categorie:"Bussen",
+            brand:"Mercedes",
+            capacity:"600",
+            license:"Rijbewijs B",
+            plug:"Nvt.",
+            size:"100x200x30",
+            restrictions:"Niet roken & drinken",
+            status:"Beschikbaar",
+            title:"Mercedes Sprinter",
+            description:" Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heef",
+            transmission:"Hand geschakeld",
+            year:"1990",
+            engine:"300pk",
+            fuel:"benzine"
+          }])
